667ee5e933e78e8cc9c1dafadf321a0d425c8721
oxkarthebest <skr_ivan@hotmail.com>
2022-12-01 11:00:48 -0500

    oscar

--- layers/layers.js
+++ layers/layers.js
@@ -59,8 +59,8 @@ var lyr_U_SECTOR_4 = new ol.layer.Vector({
                 declutter: true,
                 source:jsonSource_U_SECTOR_4, 
                 style: style_U_SECTOR_4,
-                interactive: false,
-                title: '<img src="styles/legend/U_SECTOR_4.png" /> Sector'
+                interactive: true,
+                title: '<img src="styles/legend/U_SECTOR_4.png" /> U_SECTOR'
             });
 var format_U_MANZANA_5 = new ol.format.GeoJSON();
 var features_U_MANZANA_5 = format_U_MANZANA_5.readFeatures(json_U_MANZANA_5, 
@@ -76,7 +76,7 @@ maxResolution:2.394101811356875,
  
                 style: style_U_MANZANA_5,
                 interactive: true,
-                title: '<img src="styles/legend/U_MANZANA_5.png" /> Manzana'
+                title: '<img src="styles/legend/U_MANZANA_5.png" /> U_MANZANA'
             });
 var format_U_TERRENO_6 = new ol.format.GeoJSON();
 var features_U_TERRENO_6 = format_U_TERRENO_6.readFeatures(json_U_TERRENO_6, 
@@ -92,29 +92,7 @@ maxResolution:1.197190928009199,
  
                 style: style_U_TERRENO_6,
                 interactive: true,
-                title: 'Terreno<br />\
-                -Venta<br />\
-                <img src="styles/legend/Ventas_3_0.png" /> -60000000<br />\
-                <img src="styles/legend/Ventas_3_1.png" /> 60000000 - 102727272,73<br />\
-                <img src="styles/legend/Ventas_3_2.png" /> 102727272,73 - 130000000<br />\
-                <img src="styles/legend/Ventas_3_3.png" /> 130000000 - 160000000<br />\
-                <img src="styles/legend/Ventas_3_4.png" /> 160000000 - 195909090,91<br />\
-                <img src="styles/legend/Ventas_3_5.png" /> 195909090,91 - 250000000<br />\
-                <img src="styles/legend/Ventas_3_6.png" /> 250000000 - 300000000<br />\
-                <img src="styles/legend/Ventas_3_7.png" /> 300000000 - 360000000<br />\
-                <img src="styles/legend/Ventas_3_8.png" /> 360000000 - 550000000<br />\
-                <img src="styles/legend/Ventas_3_9.png" /> 550000000 - 1975000000<br />\
-                -Arriendo<br />\
-                <img src="styles/legend/Arrendo_4_0.png" /> -250000<br />\
-                <img src="styles/legend/Arrendo_4_1.png" /> 250000 - 350000<br />\
-                <img src="styles/legend/Arrendo_4_2.png" /> 350000 - 394211<br />\
-                <img src="styles/legend/Arrendo_4_3.png" /> 394211 - 450000<br />\
-                <img src="styles/legend/Arrendo_4_4.png" /> 450000 - 550789<br />\
-                <img src="styles/legend/Arrendo_4_5.png" /> 550789 - 622632<br />\
-                <img src="styles/legend/Arrendo_4_6.png" /> 622632 - 750000<br />\
-                <img src="styles/legend/Arrendo_4_7.png" /> 750000 - 1100000<br />\
-                <img src="styles/legend/Arrendo_4_8.png" /> 1100000 - 1300000<br />\
-                <img src="styles/legend/Arrendo_4_9.png" /> 1300000 - 7500000<br />'
+                title: 'U_TERRENO'
             });
 var format_OFERTAS_OBSERVATORIO_7 = new ol.format.GeoJSON();
 var features_OFERTAS_OBSERVATORIO_7 = format_OFERTAS_OBSERVATORIO_7.readFeatures(json_OFERTAS_OBSERVATORIO_7, 
@@ -128,9 +106,7 @@ var lyr_OFERTAS_OBSERVATORIO_7 = new ol.layer.Vector({
                 source:jsonSource_OFERTAS_OBSERVATORIO_7, 
                 style: style_OFERTAS_OBSERVATORIO_7,
                 interactive: true,
-                title: 'Ofertas<br />\
-                <img src="styles/legend/Ofertas_5_1.png" /> Venta<br />\
-                <img src="styles/legend/Ofertas_5_2.png" /> Arriendo<br />'
+                title: 'OFERTAS_OBSERVATORIO'
             });
 var format_U_NOMENCLATRURA_VIAL_8 = new ol.format.GeoJSON();
 var features_U_NOMENCLATRURA_VIAL_8 = format_U_NOMENCLATRURA_VIAL_8.readFeatures(json_U_NOMENCLATRURA_VIAL_8, 
@@ -145,8 +121,8 @@ var lyr_U_NOMENCLATRURA_VIAL_8 = new ol.layer.Vector({
 maxResolution:4.787923578052228,
  
                 style: style_U_NOMENCLATRURA_VIAL_8,
-                interactive: false,
-                title: '<img src="styles/legend/U_NOMENCLATRURA_VIAL_8.png" /> Nomenclatura Vial'
+                interactive: true,
+                title: '<img src="styles/legend/U_NOMENCLATRURA_VIAL_8.png" /> U_NOMENCLATRURA_VIAL'
             });
 
 lyr_NoMap_0.setVisible(false);lyr_OpenStreetMap_1.setVisible(false);lyr_CartoDbDarkMatter_2.setVisible(false);lyr_CartoDbPositron_3.setVisible(true);lyr_U_SECTOR_4.setVisible(true);lyr_U_MANZANA_5.setVisible(true);lyr_U_TERRENO_6.setVisible(true);lyr_OFERTAS_OBSERVATORIO_7.setVisible(true);lyr_U_NOMENCLATRURA_VIAL_8.setVisible(true);
@@ -161,11 +137,11 @@ lyr_U_MANZANA_5.set('fieldImages', {'OBJECTID': 'TextEdit', 'CODIGO': 'TextEdit'
 lyr_U_TERRENO_6.set('fieldImages', {'CODIGO': 'TextEdit', 'ÁREA_M2': 'TextEdit', 'OFERTAS_OBSERVATORIO_Valor': 'TextEdit', 'OFERTAS_OBSERVATORIO_Tipo_de_Of': 'TextEdit', });
 lyr_OFERTAS_OBSERVATORIO_7.set('fieldImages', {'OBJECTID': 'TextEdit', 'CODIGO': 'TextEdit', 'TERRENO_CO': 'TextEdit', 'TIPO_CONST': 'TextEdit', 'TIPO_DOMIN': 'TextEdit', 'NUMERO_PIS': 'TextEdit', 'NUMERO_SOT': 'TextEdit', 'NUMERO_MEZ': 'TextEdit', 'NUMERO_SEM': 'TextEdit', 'CODIGO_MUN': 'TextEdit', 'COOR_X': 'TextEdit', 'COOR_Y': 'TextEdit', 'RASTERVALU': 'TextEdit', 'Numero_de_': 'TextEdit', 'Numero_Pre': 'TextEdit', 'Barrio': 'TextEdit', 'Direccion': 'TextEdit', 'Telefono': 'TextEdit', 'Valor': 'TextEdit', 'Porcentaje': 'TextEdit', 'Valor_Depu': 'TextEdit', 'Fecha': 'TextEdit', 'Area_de_Te': 'TextEdit', 'Numero_d_1': 'TextEdit', 'Numero_d_2': 'TextEdit', 'Tipo_de_Co': 'TextEdit', 'Edad_del_I': 'TextEdit', 'Servicios_': 'TextEdit', 'Servicio_1': 'TextEdit', 'Servicio_2': 'TextEdit', 'Casa_Canti': 'TextEdit', 'Casa_Can_1': 'TextEdit', 'Apto_Canti': 'TextEdit', 'Apto_Numer': 'TextEdit', 'Apto_Tiene': 'TextEdit', 'Cantidad_d': 'TextEdit', 'Garaje_Can': 'TextEdit', 'Garaje_Tip': 'TextEdit', 'Garaje_T_1': 'TextEdit', 'Tipo_de_In': 'TextEdit', 'Tipo_de_Of': 'TextEdit', 'Fuente_Inf': 'TextEdit', });
 lyr_U_NOMENCLATRURA_VIAL_8.set('fieldImages', {'OBJECTID': 'TextEdit', 'TEXTO': 'TextEdit', });
-lyr_U_SECTOR_4.set('fieldLabels', {'OBJECTID': 'inline label', 'CODIGO': 'inline label', 'USUARIO_LO': 'inline label', 'FECHA_LOG': 'inline label', 'GLOBALID': 'inline label', 'GLOBALID_S': 'inline label', 'CODIGO_MUN': 'inline label', 'SHAPE_Leng': 'inline label', 'SHAPE_Area': 'inline label', });
-lyr_U_MANZANA_5.set('fieldLabels', {'OBJECTID': 'inline label', 'CODIGO': 'inline label', 'AREA_M2': 'inline label', });
-lyr_U_TERRENO_6.set('fieldLabels', {'CODIGO': 'inline label', 'ÁREA_M2': 'inline label', 'OFERTAS_OBSERVATORIO_Valor': 'inline label', 'OFERTAS_OBSERVATORIO_Tipo_de_Of': 'inline label', });
-lyr_OFERTAS_OBSERVATORIO_7.set('fieldLabels', {'OBJECTID': 'inline label', 'CODIGO': 'inline label', 'TERRENO_CO': 'inline label', 'TIPO_CONST': 'inline label', 'TIPO_DOMIN': 'inline label', 'NUMERO_PIS': 'inline label', 'NUMERO_SOT': 'inline label', 'NUMERO_MEZ': 'inline label', 'NUMERO_SEM': 'inline label', 'CODIGO_MUN': 'inline label', 'COOR_X': 'inline label', 'COOR_Y': 'inline label', 'RASTERVALU': 'inline label', 'Numero_de_': 'inline label', 'Numero_Pre': 'inline label', 'Barrio': 'inline label', 'Direccion': 'inline label', 'Telefono': 'inline label', 'Valor': 'inline label', 'Porcentaje': 'inline label', 'Valor_Depu': 'inline label', 'Fecha': 'inline label', 'Area_de_Te': 'inline label', 'Numero_d_1': 'inline label', 'Numero_d_2': 'inline label', 'Tipo_de_Co': 'inline label', 'Edad_del_I': 'inline label', 'Servicios_': 'inline label', 'Servicio_1': 'inline label', 'Servicio_2': 'inline label', 'Casa_Canti': 'inline label', 'Casa_Can_1': 'inline label', 'Apto_Canti': 'inline label', 'Apto_Numer': 'inline label', 'Apto_Tiene': 'inline label', 'Cantidad_d': 'inline label', 'Garaje_Can': 'inline label', 'Garaje_Tip': 'inline label', 'Garaje_T_1': 'inline label', 'Tipo_de_In': 'inline label', 'Tipo_de_Of': 'inline label', 'Fuente_Inf': 'inline label', });
-lyr_U_NOMENCLATRURA_VIAL_8.set('fieldLabels', {'OBJECTID': 'inline label', 'TEXTO': 'inline label', });
+lyr_U_SECTOR_4.set('fieldLabels', {'OBJECTID': 'no label', 'CODIGO': 'no label', 'USUARIO_LO': 'no label', 'FECHA_LOG': 'no label', 'GLOBALID': 'no label', 'GLOBALID_S': 'no label', 'CODIGO_MUN': 'no label', 'SHAPE_Leng': 'no label', 'SHAPE_Area': 'no label', });
+lyr_U_MANZANA_5.set('fieldLabels', {'OBJECTID': 'no label', 'CODIGO': 'no label', 'AREA_M2': 'no label', });
+lyr_U_TERRENO_6.set('fieldLabels', {'CODIGO': 'no label', 'ÁREA_M2': 'no label', 'OFERTAS_OBSERVATORIO_Valor': 'no label', 'OFERTAS_OBSERVATORIO_Tipo_de_Of': 'no label', });
+lyr_OFERTAS_OBSERVATORIO_7.set('fieldLabels', {'OBJECTID': 'no label', 'CODIGO': 'no label', 'TERRENO_CO': 'no label', 'TIPO_CONST': 'no label', 'TIPO_DOMIN': 'no label', 'NUMERO_PIS': 'no label', 'NUMERO_SOT': 'no label', 'NUMERO_MEZ': 'no label', 'NUMERO_SEM': 'no label', 'CODIGO_MUN': 'no label', 'COOR_X': 'no label', 'COOR_Y': 'no label', 'RASTERVALU': 'no label', 'Numero_de_': 'no label', 'Numero_Pre': 'no label', 'Barrio': 'no label', 'Direccion': 'no label', 'Telefono': 'no label', 'Valor': 'no label', 'Porcentaje': 'no label', 'Valor_Depu': 'no label', 'Fecha': 'no label', 'Area_de_Te': 'no label', 'Numero_d_1': 'no label', 'Numero_d_2': 'no label', 'Tipo_de_Co': 'no label', 'Edad_del_I': 'no label', 'Servicios_': 'no label', 'Servicio_1': 'no label', 'Servicio_2': 'no label', 'Casa_Canti': 'no label', 'Casa_Can_1': 'no label', 'Apto_Canti': 'no label', 'Apto_Numer': 'no label', 'Apto_Tiene': 'no label', 'Cantidad_d': 'no label', 'Garaje_Can': 'no label', 'Garaje_Tip': 'no label', 'Garaje_T_1': 'no label', 'Tipo_de_In': 'no label', 'Tipo_de_Of': 'no label', 'Fuente_Inf': 'no label', });
+lyr_U_NOMENCLATRURA_VIAL_8.set('fieldLabels', {'OBJECTID': 'no label', 'TEXTO': 'no label', });
 lyr_U_NOMENCLATRURA_VIAL_8.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
